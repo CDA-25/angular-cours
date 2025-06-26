e0fc8ed14fbc6b05d608e97360b3bcfd99719add
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-06-26 11:46:02 +0200

    ajout cours

--- cours/src/app/app.ts
+++ cours/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test } from "./test/test";
+import { Footer } from "./footer/footer";
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
